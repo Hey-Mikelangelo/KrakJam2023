63ac36e179341b1c2efc11276b864c4799d4d5f4
MichalSipkovski <michal.sipkovski.work@gmail.com>
2023-02-05 12:23:21 +0100

    Added age and name logic

---
+++ Assets/Scripts/ColorPuzzle/IntValueSOVariable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine.Localization.SmartFormat.Core.Extensions;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
+
+[Serializable]
+public class IntValueSOVariable : IVariable, IVariableValueChanged
+{
+    public IntValueSO intValueSO;
+
+    public event Action<IVariable> ValueChanged;
+
+    
+    public object GetSourceValue(ISelectorInfo selector)
+    {
+        intValueSO.OnValueChanged -= IntValueSO_OnValueChanged;
+        intValueSO.OnValueChanged += IntValueSO_OnValueChanged;
+        return intValueSO.IsNullWithErrorLog() ? "Null" : intValueSO.Value.ToString();
+    }
+
+    private void IntValueSO_OnValueChanged()
+    {
+        ValueChanged.Invoke(this);
+    }
+}
+
